f5b9adb53f967c0bbb3b58b86cc95bfc33cfb5ad
Leo Mata <>
2026-03-29 22:30:49 -0600

    fix: add TypeScript parser for Svelte ESLint, disable navigation/key rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,29 @@
+import tsParser from '@typescript-eslint/parser';
 import eslintPluginSvelte from 'eslint-plugin-svelte';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import svelteParser from 'svelte-eslint-parser';
 
 export default [
 	{
 		ignores: ['.svelte-kit/**', 'build/**', 'node_modules/**', 'drizzle/**']
 	},
 	...eslintPluginSvelte.configs['flat/recommended'],
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser: tsParser,
+			},
+		},
+	},
 	eslintConfigPrettier,
 	{
 		rules: {
 			'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-			'svelte/no-at-html-tags': 'off'
+			'svelte/no-at-html-tags': 'off',
+			'svelte/no-navigation-without-resolve': 'off',
+			'svelte/require-each-key': 'warn',
 		}
 	}
 ];
